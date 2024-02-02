b085bb6019f1eef96cefd3c13c89c987c99f2b5c
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-02 13:32:35 +0200

    Creating flyway migratio and application modules

---
+++ app/src/main/java/org/example/seccourse/SpringSecApplication.java
@@ -0,0 +1,11 @@
+package org.example.seccourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecApplication.class);
+    }
+}
