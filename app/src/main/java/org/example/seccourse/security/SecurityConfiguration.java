23c2ac420b872544bdfb6c3bea61c881c33b8dc1
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-03 11:35:30 +0200

    Authentication functionality

--- app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
+++ app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
@@ -1,19 +1,36 @@
 package org.example.seccourse.security;
 
+import org.example.seccourse.repository.UserRepository;
+import org.example.seccourse.service.AuthenticationUserService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/api/**").permitAll()
+                        .anyRequest().authenticated());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return new AuthenticationUserService(userRepository);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
