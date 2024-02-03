442465fafa61012c7dd3be01eacbe1b6410ccf31
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-03 15:38:31 +0200

    Adding permission

--- app/src/main/java/org/example/seccourse/model/enums/Permissions.java
+++ app/src/main/java/org/example/seccourse/model/enums/Permissions.java
@@ -1,18 +1,17 @@
 package org.example.seccourse.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Permissions {
     STUDENT_READ("student:read"),
     STUDENT_WRITE("student:write"),
     COURSE_READ("course:read"),
     COURSE_WRITE("course:write");
 
-    public final String permission;
+    private final String permission;
 
     Permissions(String permission) {
         this.permission = permission;
-    }
-
-    public String getPermission(){
-        return this.permission;
     }
 }
