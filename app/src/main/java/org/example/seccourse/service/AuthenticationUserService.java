4fbdb0abb5f212548b27fa6fbcab23f82f2b5672
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-06 12:44:18 +0200

    authentication

--- app/src/main/java/org/example/seccourse/service/AuthenticationUserService.java
+++ app/src/main/java/org/example/seccourse/service/AuthenticationUserService.java
@@ -1,7 +1,6 @@
 package org.example.seccourse.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.seccourse.model.entity.UserEntity;
 import org.example.seccourse.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +12,8 @@ public class AuthenticationUserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = this.userRepository
+        return this.userRepository
                 .findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User with username: " + username + " does not exist!"));
-
-        return user;
     }
 }
