565af87ede6b68ccf90eb7598535dda9f32afdeb
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-06 19:07:07 +0200

    Jwt auth

--- app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
+++ app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
@@ -1,27 +1,35 @@
 package org.example.seccourse.security;
 
+import lombok.RequiredArgsConstructor;
+import org.example.seccourse.jwt.JwtUsernamePasswordAuthenticationFilter;
 import org.example.seccourse.repository.UserRepository;
 import org.example.seccourse.service.AuthenticationUserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.example.seccourse.model.enums.Role.STUDENT;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationConfiguration config) throws Exception {
+        httpSecurity.addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager(config)))
+                .authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/", "/login").permitAll()
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
 //                        .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
@@ -29,14 +37,16 @@ public class SecurityConfiguration {
 //                        .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                        .requestMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest().authenticated())
-                .formLogin(login -> login.loginPage("/login").defaultSuccessUrl("/courses", true))
-                .rememberMe(r -> r.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                        .key("topsecretkey"))
-                .logout(logout -> logout.logoutUrl("/logout")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID", "remember-me")
-                        .logoutSuccessUrl("/"));
+//                .formLogin(login -> login.loginPage("/login").defaultSuccessUrl("/courses", true))
+//                .rememberMe(r -> r.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+//                        .key("topsecretkey"))
+//                .logout(logout -> logout.logoutUrl("/logout")
+//                        .invalidateHttpSession(true)
+//                        .clearAuthentication(true)
+//                        .deleteCookies("JSESSIONID", "remember-me")
+//                        .logoutSuccessUrl("/"))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(CsrfConfigurer::disable);
 
         return httpSecurity.build();
     }
@@ -72,6 +82,11 @@ public class SecurityConfiguration {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
