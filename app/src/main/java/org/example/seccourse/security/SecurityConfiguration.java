95350b6468b2b2b1e7c8bdfef25fa45f52135438
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-06 12:57:17 +0200

    authentication

--- app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
+++ app/src/main/java/org/example/seccourse/security/SecurityConfiguration.java
@@ -5,64 +5,66 @@ import org.example.seccourse.service.AuthenticationUserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static org.example.seccourse.model.enums.Role.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.example.seccourse.model.enums.Role.STUDENT;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/").permitAll()
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/login").permitAll()
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
 //                        .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                        .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                        .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                        .requestMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest().authenticated())
-                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+                .formLogin(login -> login.loginPage("/login").defaultSuccessUrl("/courses", true))
+                .rememberMe(r -> r.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                        .key("topsecretkey"))
+                .logout(logout -> logout.logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .logoutSuccessUrl("/"));
 
         return httpSecurity.build();
     }
 
     @Bean
     UserDetailsService userDetailsService(UserRepository userRepository) {
-        UserDetails student = User.builder()
-                .username("Kalin")
-                .password(passwordEncoder().encode("123"))
-                //.roles(STUDENT.name())
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("Admin")
-                .password(passwordEncoder().encode("123"))
-                //.roles(ADMIN.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails adminT = User.builder()
-                .username("AdminT")
-                .password(passwordEncoder().encode("123"))
-                //.roles(ADMINTRAINEE.name())
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        //return new InMemoryUserDetailsManager(student, admin, adminT);
+//        UserDetails student = User.builder()
+//                .username("Kalin")
+//                .password(passwordEncoder().encode("123"))
+//                //.roles(STUDENT.name())
+//                .authorities(STUDENT.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("Admin")
+//                .password(passwordEncoder().encode("123"))
+//                //.roles(ADMIN.name())
+//                .authorities(ADMIN.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails adminT = User.builder()
+//                .username("AdminT")
+//                .password(passwordEncoder().encode("123"))
+//                //.roles(ADMINTRAINEE.name())
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//        //return new InMemoryUserDetailsManager(student, admin, adminT);
 
         return new AuthenticationUserService(userRepository);
     }
@@ -71,4 +73,5 @@ public class SecurityConfiguration {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
